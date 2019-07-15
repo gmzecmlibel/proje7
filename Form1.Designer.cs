224e6f66be94e9f2304665b8f42ea973015b5c3a
gmzecmlibel <48735499+gmzecmlibel@users.noreply.github.com>
2019-07-15 22:41:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace WindowsFormsApplication9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.stokidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stoknameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitpriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.catidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stokBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ticari = new WindowsFormsApplication9.Ticari();
+            this.stokTableAdapter = new WindowsFormsApplication9.TicariTableAdapters.stokTableAdapter();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stokBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticari)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button10);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button9);
+            this.splitContainer1.Panel1.Controls.Add(this.button8);
+            this.splitContainer1.Panel1.Controls.Add(this.button7);
+            this.splitContainer1.Panel1.Controls.Add(this.button6);
+            this.splitContainer1.Panel1.Controls.Add(this.button5);
+            this.splitContainer1.Panel1.Controls.Add(this.button4);
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(674, 324);
+            this.splitContainer1.SplitterDistance = 110;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(512, 80);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.ImageIndex = 1;
+            this.button7.ImageList = this.ımageList1;
+            this.button7.Location = new System.Drawing.Point(351, 13);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(49, 38);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "iconfinder_document-new_118910.png");
+            this.ımageList1.Images.SetKeyName(1, "iconfinder_edit-delete_118920.png");
+            this.ımageList1.Images.SetKeyName(2, "iconfinder_floppy_285657.png");
+            this.ımageList1.Images.SetKeyName(3, "iconfinder_go-first_118769.png");
+            this.ımageList1.Images.SetKeyName(4, "iconfinder_go-last_118772.png");
+            this.ımageList1.Images.SetKeyName(5, "iconfinder_go-next_118773.png");
+            this.ımageList1.Images.SetKeyName(6, "iconfinder_go-previous_118774.png");
+            this.ımageList1.Images.SetKeyName(7, "iconfinder_icon-136-document-edit_314724.png");
+            this.ımageList1.Images.SetKeyName(8, "iconfinder_magnifyingglass_1055031.png");
+            // 
+            // button6
+            // 
+            this.button6.ImageIndex = 7;
+            this.button6.ImageList = this.ımageList1;
+            this.button6.Location = new System.Drawing.Point(296, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 38);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.ImageIndex = 0;
+            this.button5.ImageList = this.ımageList1;
+            this.button5.Location = new System.Drawing.Point(241, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 38);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.ImageIndex = 4;
+            this.button4.ImageList = this.ımageList1;
+            this.button4.Location = new System.Drawing.Point(188, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(49, 38);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.ImageIndex = 6;
+            this.button3.ImageList = this.ımageList1;
+            this.button3.Location = new System.Drawing.Point(133, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(49, 38);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.ImageIndex = 5;
+            this.button2.ImageList = this.ımageList1;
+            this.button2.Location = new System.Drawing.Point(78, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 38);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ImageIndex = 3;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 38);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.stokidDataGridViewTextBoxColumn,
+            this.stoknameDataGridViewTextBoxColumn,
+            this.unitpriceDataGridViewTextBoxColumn,
+            this.catidDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.stokBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(674, 210);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // stokidDataGridViewTextBoxColumn
+            // 
+            this.stokidDataGridViewTextBoxColumn.DataPropertyName = "stok_id";
+            this.stokidDataGridViewTextBoxColumn.HeaderText = "stok_id";
+            this.stokidDataGridViewTextBoxColumn.Name = "stokidDataGridViewTextBoxColumn";
+            this.stokidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // stoknameDataGridViewTextBoxColumn
+            // 
+            this.stoknameDataGridViewTextBoxColumn.DataPropertyName = "stok_name";
+            this.stoknameDataGridViewTextBoxColumn.HeaderText = "stok_name";
+            this.stoknameDataGridViewTextBoxColumn.Name = "stoknameDataGridViewTextBoxColumn";
+            // 
+            // unitpriceDataGridViewTextBoxColumn
+            // 
+            this.unitpriceDataGridViewTextBoxColumn.DataPropertyName = "unit_price";
+            this.unitpriceDataGridViewTextBoxColumn.HeaderText = "unit_price";
+            this.unitpriceDataGridViewTextBoxColumn.Name = "unitpriceDataGridViewTextBoxColumn";
+            // 
+            // catidDataGridViewTextBoxColumn
+            // 
+            this.catidDataGridViewTextBoxColumn.DataPropertyName = "cat_id";
+            this.catidDataGridViewTextBoxColumn.HeaderText = "cat_id";
+            this.catidDataGridViewTextBoxColumn.Name = "catidDataGridViewTextBoxColumn";
+            // 
+            // stokBindingSource
+            // 
+            this.stokBindingSource.DataMember = "stok";
+            this.stokBindingSource.DataSource = this.ticari;
+            // 
+            // ticari
+            // 
+            this.ticari.DataSetName = "Ticari";
+            this.ticari.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // stokTableAdapter
+            // 
+            this.stokTableAdapter.ClearBeforeFill = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(593, 80);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(445, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(551, 20);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "ara";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 324);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stokBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticari)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Ticari ticari;
+        private System.Windows.Forms.BindingSource stokBindingSource;
+        private TicariTableAdapters.stokTableAdapter stokTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stokidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stoknameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitpriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn catidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'ticari.stok' table. You can move, or remove it, as needed.
+            this.stokTableAdapter.Fill(this.ticari.stok);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //DataRow r = ticari.stok.NewRow();
+            //r["stok_id"]  r[0]
+            Ticari.stokRow r = ticari.stok.NewstokRow();
+            //r["stok_id"]  r[0]
+            r.stok_name = "deneme";
+            r.unit_price = 1250;
+            r.cat_id = 1;
+            ticari.stok.AddstokRow(r);
+            stokTableAdapter.Update(r);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int id = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+
+
+            Ticari.stokRow r = ticari.stok.FindBystok_id(id);
+            r.unit_price = 1.1 * r.unit_price;
+            r.stok_name = r.stok_name.ToUpper();
+            stokTableAdapter.Update(r);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // Ticari.stokRow r = ticari.stok.FindBystok_id(5);
+
+            int id = (int)dataGridView1.SelectedRows[0].Cells[0].Value;
+
+
+            Ticari.stokRow r = ticari.stok.FindBystok_id(id);
+
+            r.Delete();
+            stokTableAdapter.Update(r);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow  r in ticari.stok.Rows )
+            {
+                r.Delete();
+            }
+
+            stokTableAdapter.Update(ticari.stok);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            foreach (DataGridViewRow item in dataGridView1.SelectedRows)
+            {
+                int id = (int)item.Cells[0].Value;
+
+                Ticari.stokRow r = ticari.stok.FindBystok_id(id);
+
+                r.Delete();
+            }
+            stokTableAdapter.Update(ticari.stok);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stokBindingSource.MoveFirst();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stokBindingSource.MoveNext();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stokBindingSource.MovePrevious();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stokBindingSource.MoveLast();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int id = stokBindingSource.Find("stok_name", textBox1.Text);
+                       
+            stokBindingSource.Position = id;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string   selstr=" stok_name like'"+textBox1.Text+"%'";
+                       
+            // stokBindingSource.Filter = selstr;  //  bu kullanılırsa kayıtlar filtrelenir  aşağıdakilere gerek kalmaz 
+            DataRow[] secilirows = ticari.stok.Select(selstr);
+            if (secilirows.Length != 0)
+                stokBindingSource.Position = stokBindingSource.Find("stok_name", secilirows[0]["stok_name"]);
+
+
+            
+        }
+    }
+}
